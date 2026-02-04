ff97e64a87299b322ea74cc70291f29d5f9b640f
shenhua3485 <shen.hua@hellogroup.com>
2026-02-04 16:44:42 +0800

    call hello method

--- HotUpdateProject/Assets/HybridCLR/LoadDll.cs
+++ HotUpdateProject/Assets/HybridCLR/LoadDll.cs
@@ -21,5 +21,7 @@ public class LoadDll : MonoBehaviour
         // Editor下无需加载，直接查找获得HotUpdate程序集
         Assembly hotUpdateAss = System.AppDomain.CurrentDomain.GetAssemblies().First(a => a.GetName().Name == "HotUpdate");
 #endif
+        Type type = hotUpdateAss.GetType("Hello");
+        type.GetMethod("Run").Invoke(null, null);
     }
 }
