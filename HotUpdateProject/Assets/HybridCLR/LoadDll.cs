91b674fcb23bce9ad288608703fcba9744490cdc
shenhua3485 <shen.hua@hellogroup.com>
2026-02-04 19:12:31 +0800

    load metadata from aot assemblies

--- HotUpdateProject/Assets/HybridCLR/LoadDll.cs
+++ HotUpdateProject/Assets/HybridCLR/LoadDll.cs
@@ -11,15 +11,40 @@ using UnityEngine.Networking;
 
 public class LoadDll : MonoBehaviour
 {
+    private void Start()
+    {
+        LoadMetadataForAOTAssemblies();
+    }
+
+    private static void LoadMetadataForAOTAssemblies()
+    {
+        List<string> aotDllList = new List<string>
+        {
+            "mscorlib.dll",
+            "System.dll",
+            "System.Core.dll", // 如果使用了Linq，需要这个
+            // "Newtonsoft.Json.dll", 
+            // "protobuf-net.dll",
+        };
+
+        foreach (var aotDllName in aotDllList)
+        {
+            byte[] dllBytes = File.ReadAllBytes($"{Application.streamingAssetsPath}/{aotDllName}.bytes");
+            LoadImageErrorCode err = HybridCLR.RuntimeApi.LoadMetadataForAOTAssembly(dllBytes, HomologousImageMode.SuperSet);
+            Debug.Log($"LoadMetadataForAOTAssembly:{aotDllName}. ret:{err}");
+        }
+    }
 
     public void StartLoadDll()
     {
         // Editor环境下，HotUpdate.dll.bytes已经被自动加载，不需要加载，重复加载反而会出问题。
 #if !UNITY_EDITOR
-        Assembly hotUpdateAss = Assembly.Load(File.ReadAllBytes($"{Application.streamingAssetsPath}/HotUpdate.dll.bytes"));
+        Assembly hotUpdateAss =
+ Assembly.Load(File.ReadAllBytes($"{Application.streamingAssetsPath}/HotUpdate.dll.bytes"));
 #else
         // Editor下无需加载，直接查找获得HotUpdate程序集
-        Assembly hotUpdateAss = System.AppDomain.CurrentDomain.GetAssemblies().First(a => a.GetName().Name == "HotUpdate");
+        Assembly hotUpdateAss =
+            System.AppDomain.CurrentDomain.GetAssemblies().First(a => a.GetName().Name == "HotUpdate");
 #endif
         Type type = hotUpdateAss.GetType("Hello");
         type.GetMethod("Run").Invoke(null, null);
